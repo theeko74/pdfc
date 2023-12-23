6186378287103aa0cf51e1a51b7d3bf561d6076a
Artem Zaborshikov <31626137+zaborshikov@users.noreply.github.com>
2023-12-23 04:30:28 +0300

    Add lvls of compress

--- gui.py
+++ gui.py
@@ -2,17 +2,20 @@ import flet as ft
 from compresser import main as compressor_main
 
 input_path = ''
+lvl = 2
 
 def main(page: ft.Page):
-    page.window_width = 400        # window's width is 200 px
-    page.window_height = 300       # window's height is 200 px
+    page.window_width = 500        # window's width is 200 px
+    page.window_height = 350       # window's height is 200 px
     page.update()
     
+    def radiogroup_changed(e):
+        global lvl
+        lvl = int(e.control.value)
+        
     def pick_files_result(e: ft.FilePickerResultEvent):
         global input_path
-        selected_files.value = (
-            ", ".join(map(lambda f: f.name, e.files)) if e.files else "Cancelled!"
-        )
+        selected_files.value = e.files[0].path
         input_path = e.files[0].path
         selected_files.update()
     
@@ -27,7 +30,7 @@ def main(page: ft.Page):
             t.value = 'No input path'
             page.update()
         else:
-            flag, ratio, size = compressor_main(input_path, outpth, remove=chechboxes[0].value)
+            flag, ratio, size = compressor_main(input_path, outpth, remove=chechboxes[0].value, compress_lvl=lvl)
             if flag:
                 t.value = f'File compressed by {ratio:.2%}, final size is {size} MB.'
                 page.update()
@@ -57,12 +60,19 @@ def main(page: ft.Page):
         )
     )
     
+    select_lvl_txt = ft.Text('Select level of compression')
+    cg = ft.RadioGroup(content=ft.Row([ft.Radio(value=i, label=i) for i in range(1, 5)]), on_change=radiogroup_changed)
+    cg.value = 2
+    page.add(select_lvl_txt, cg)
+    
     chechboxes = [ft.Checkbox(label=cbox[0], value=cbox[1]) for cbox in (('Remove original file after compress', False),)]
     b = ft.ElevatedButton(text="Compress", on_click=button_clicked)
     page.add(*chechboxes, b)
     
+    
     t = ft.Text()
     page.add(t)
+
     
 
 
